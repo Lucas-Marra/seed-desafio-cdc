2ddc671e57716bb406d4018105f2df0d9b430134
Lucas Rebello <lucasmrebello@yahoo.com.br>
2025-11-07 23:40:27 -0300

    refactor(author): using only entity manager to persist on database

--- src/main/java/br/com/lucas/casadocodigo/author/AuthorController.java
+++ src/main/java/br/com/lucas/casadocodigo/author/AuthorController.java
@@ -1,33 +1,27 @@
 package br.com.lucas.casadocodigo.author;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/author")
 public class AuthorController {
 
-    private AuthorRepository authorRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    @Autowired
-    public AuthorController(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
-
+    @Transactional
     @PostMapping("/new")
     public ResponseEntity<?> createAuthor(@RequestBody @Valid AuthorRequest authorRequest) {
-        Author author = authorRepository.save(authorRequest.toModel());
+        entityManager.persist(authorRequest.toModel());
 
-        return ResponseEntity
-                .created(URI.create("/author/" + author.getId()))
-                .body(Map.of("id", author.getId()));
+        return ResponseEntity.ok().build();
     }
 }
